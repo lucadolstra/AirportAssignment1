ebdaf0768e6246aed199b5b11f90a427f8f32496
lucadolstra <38104438+lucadolstra@users.noreply.github.com>
2018-05-15 13:00:55 +0200

    fix

--- src/main/java/com/capgemini/airport1/repository/AirportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.capgemini.airport1.repository;
-
-import com.capgemini.airport1.model.Airport;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AirportRepository extends CrudRepository<Airport, Long> {
-}
